7ba7d776f89d473f5534e0612c60dc5abd8d0acf
chethtra yen <chethtra_yen@rocketmail.com>
2017-03-24 19:52:19 -0400

    added http thingy

---
+++ chet_Assets/Response.cs
@@ -0,0 +1,19 @@
+﻿
+/**
+* @purpose Json serialization (Json -> Object) 
+**/
+
+public class Response {
+
+	public string boii;
+  public string error;
+
+  /**
+  * Constructor 
+  **/
+  public Response (string error) {
+    this.error = error;
+  }
+
+
+}
